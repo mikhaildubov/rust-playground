10ad97bf2455e88289720d53fd194b0c6260eeb0
Mikhail Dubov <msdubov@gmail.com>
2019-03-14 15:21:00 +0000

    Add some examples of constants and shadowing

--- rust-lang-book/ch03-01-variables-and-mutability/src/main.rs
+++ rust-lang-book/ch03-01-variables-and-mutability/src/main.rs
@@ -1,3 +1,5 @@
+const TEN_THOUSAND: u32 = 10_000;
+
 fn test_raw_identifiers() {
     let r#fn = "test";
     //let fn = "test";
@@ -12,7 +14,25 @@ fn test_immutable_variables() {
     println!("The value of x is: {}", x);
 }
 
+fn test_constants() {
+    println!("The value of TEN_THOUSAND is: {}", TEN_THOUSAND);
+}
+
+fn test_shadowing() {
+    let x = 5;
+    //x = x + 1;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";         // string
+    let spaces = spaces.len();  // integer
+    println!("The value of spaces is: {}", spaces);
+}
+
 fn main() {
     test_raw_identifiers();
     test_immutable_variables();
+    test_constants();
+    test_shadowing();
 }
