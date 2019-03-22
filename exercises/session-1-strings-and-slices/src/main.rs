69d3d9c10407cd60ecc5804f936015b726f6367b
Mikhail Dubov <msdubov@gmail.com>
2019-03-22 12:07:46 +0000

    Fix a typo
    indicies -> indices

--- exercises/session-1-strings-and-slices/src/main.rs
+++ exercises/session-1-strings-and-slices/src/main.rs
@@ -6,8 +6,8 @@
 
 #![crate_type = "lib"]
 
-/// Find indicies into `text` which point to the start of substrings equal to `subject`
-pub fn find_substring_indicies(text: &str, subject: &str) -> Vec<usize> {
+/// Find indices into `text` which point to the start of substrings equal to `subject`
+pub fn find_substring_indices(text: &str, subject: &str) -> Vec<usize> {
     let mut indices = Vec::new();
 
     for i in 0..(text.len() - subject.len() + 1) {
@@ -24,7 +24,7 @@ pub fn find_substring_indicies(text: &str, subject: &str) -> Vec<usize> {
 fn simple_test1() {
     let text = "ababacaba";
     let subject = "ab";
-    let result = find_substring_indicies(text, subject);
+    let result = find_substring_indices(text, subject);
     assert_eq!(result, vec![0, 2, 6]);
 }
 
@@ -32,14 +32,14 @@ fn simple_test1() {
 fn simple_test2() {
     let text = "aaabaa";
     let subject = "aa";
-    let result = find_substring_indicies(text, subject);
+    let result = find_substring_indices(text, subject);
     assert_eq!(result, vec![0, 1, 4]);
 }
 
 // Part 2: anagrams
 
-/// Find indicies into `text` which point to the start of anagrams of `subject`
-pub fn find_anagram_indicies(text: &str, subject: &str) -> Vec<usize> {
+/// Find indices into `text` which point to the start of anagrams of `subject`
+pub fn find_anagram_indices(text: &str, subject: &str) -> Vec<usize> {
     let mut indices = Vec::new();
     let mut subject_sorted: Vec<char> = subject.chars().collect();
     subject_sorted.sort();
@@ -60,7 +60,7 @@ pub fn find_anagram_indicies(text: &str, subject: &str) -> Vec<usize> {
 fn simple_test3() {
     let text = "cbaebabacd";
     let subject = "abc";
-    let result = find_anagram_indicies(text, subject);
+    let result = find_anagram_indices(text, subject);
     assert_eq!(result, vec![0, 6]);
 }
 
@@ -68,7 +68,7 @@ fn simple_test3() {
 fn simple_test4() {
     let text = "acbaebabacd";
     let subject = "abc";
-    let result = find_anagram_indicies(text, subject);
+    let result = find_anagram_indices(text, subject);
     assert_eq!(result, vec![0, 1, 7]);
 }
 
